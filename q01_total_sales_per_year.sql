75b8a4f6fdca4946f530ab13395e75bf247efc2f
KidusDagnew <KidusDagnew0@Gmail.com>
2025-10-03 00:14:09 -0700

    Add working SQL for Q01

---
+++ q01_total_sales_per_year.sql
@@ -0,0 +1,7 @@
+/* Q01: List total sales amount (TotalDue) for each year from Sales.SalesOrderHeader, oldest→newest. */
+
+Select sum(totaldue) as 'Total Sales Amount For The Year ',
+        year(orderdate) as  'Order Date'
+from sales.SalesOrderHeader
+group by year(orderdate) 
+
