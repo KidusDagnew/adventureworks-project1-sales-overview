aadb4f66f96135a858df7dd4854f54cd6ed5348c
KidusDagnew <kidusdagnew0@gmail.com>
2025-10-04 21:17:24 -0700

    Update and rename q02_top_10_products_by_revenue.sql to Q02_top_10_products_by_revenue.sql

---
+++ Q02_top_10_products_by_revenue.sql
@@ -0,0 +1,12 @@
+/* Q02: List the top 10 products by total revenue.
+   Uses: JOIN, SUM, GROUP BY, TOP, ORDER BY
+   Output: ProductName, TotalRevenue*/
+
+Select TOP 10
+        P.name as 'ProductName',
+        sum(SO.linetotal) as 'totalRevenue' 
+from Production.Product p 
+left join sales.SalesOrderDetail SO 
+    on P.productid = SO.productid
+group by P.name 
+order by totalRevenue desc; 
